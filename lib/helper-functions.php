3d8411440103c5c99d0c5db0035199ec16e178b6
Jonathan Lau <jon@jon.sg>
2022-04-09 22:50:34 +0800

    Simply dec to hex conversion.

--- lib/helper-functions.php
+++ lib/helper-functions.php
@@ -47,8 +47,6 @@ function carmel_color_brightness( $color, $change ) {
 	$green = max( 0, min( 255, $green + $change ) );
 	$blue  = max( 0, min( 255, $blue + $change ) );
 
-	$hex = '%02X';
-
-	return '#' . sprintf( $hex, $red ) . sprintf( $hex, $green ) . sprintf( $hex, $blue );
+	return sprintf( '#%02x%02x%02x', $red, $green, $blue );
 
 }
