6d47b97a0ebe90951ef5a3e2455dd3b83fb0656b
Jonathan Lau <jon@jon.sg>
2022-04-11 00:48:06 +0800

    Remove carmel_logo_width control.

--- lib/customize.php
+++ lib/customize.php
@@ -60,49 +60,4 @@ function carmel_customizer_register( $wp_customize ) {
 		)
 	);
 
-	$wp_customize->add_setting(
-		'carmel_logo_width',
-		[
-			'default'           => 350,
-			'sanitize_callback' => 'absint',
-			'validate_callback' => 'carmel_validate_logo_width',
-		]
-	);
-
-	// Add a control for the logo size.
-	$wp_customize->add_control(
-		'carmel_logo_width',
-		[
-			'label'       => __( 'Logo Width', 'carmel' ),
-			'description' => __( 'The maximum width of the logo in pixels.', 'carmel' ),
-			'priority'    => 9,
-			'section'     => 'title_tagline',
-			'settings'    => 'carmel_logo_width',
-			'type'        => 'number',
-			'input_attrs' => [
-				'min' => 100,
-			],
-
-		]
-	);
-
-}
-
-/**
- * Displays a message if the entered width is not numeric or greater than 100.
- *
- * @param object $validity The validity status.
- * @param int    $width The width entered by the user.
- * @return int The new width.
- */
-function carmel_validate_logo_width( $validity, $width ) {
-
-	if ( empty( $width ) || ! is_numeric( $width ) ) {
-		$validity->add( 'required', __( 'You must supply a valid number.', 'carmel' ) );
-	} elseif ( $width < 100 ) {
-		$validity->add( 'logo_too_small', __( 'The logo width cannot be less than 100.', 'carmel' ) );
-	}
-
-	return $validity;
-
 }
