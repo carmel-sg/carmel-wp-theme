59180cd4bbf625fc8ff5fcfa7160c5a49e465092
Jonathan Lau <jon@jon.sg>
2022-04-16 12:46:13 +0800

    Remove sidebar-featured image size.

--- functions.php
+++ functions.php
@@ -156,7 +156,6 @@ function carmel_post_type_support() {
 }
 
 // Adds image sizes.
-add_image_size( 'sidebar-featured', 75, 75, true );
 add_image_size( 'genesis-singular-images', 702, 526, true );
 
 // Removes header right widget area.
